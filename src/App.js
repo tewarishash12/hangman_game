eeddccd0a3f505b6b33507ac27c25faa569bd806
tewarishash12 <tewarishashwat2002@gmail.com>
2024-12-25 17:28:44 +0530

    created a separate slice for wordSlice

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import './App.css';
-import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Hangman from './pages/Hangman';
 import HighScore from './pages/HighScore';
 import HomePage from './pages/Homepage';
 import { useState } from 'react';
+import Sidebar from './components/Sidebar';
 
 function App() {
   const [isCollapsed, setIsCollapsed] = useState(false);
+
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />} />
-        <Route path="/playgame" element={<Hangman isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />} />
-        <Route path="/highscore" element={<HighScore isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />} />
-      </Routes>
+      <div className="flex h-screen">
+        {/* Sidebar */}
+        <Sidebar isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
+
+        {/* Main Content */}
+        <div className="flex-grow h-full transition-all duration-300">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/playgame" element={<Hangman />} />
+            <Route path="/highscore" element={<HighScore />} />
+          </Routes>
+        </div>
+      </div>
     </Router>
   );
 }

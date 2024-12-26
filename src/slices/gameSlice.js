68bdbb5b98005c35e9c14c878ed7d50407f16af5
tewarishash12 <tewarishashwat2002@gmail.com>
2024-12-26 10:46:19 +0530

    fixed the issue caused by using allGuesses

--- src/slices/gameSlice.js
+++ src/slices/gameSlice.js
@@ -5,52 +5,56 @@ const gameSlice = createSlice({
     name: "game",
     initialState: {
         currentWord: null,
-        blanks:"",
+        blanks: "",
         lives: 4,
         allGuesses: [],
         score: 0
     },
     reducers: {
-        initialize_game: (state) =>{
-            const randomWord = gameQues[Math.floor(Math.random()*gameQues.length)]
+        initialize_game: (state) => {
+            const randomWord = gameQues[Math.floor(Math.random() * gameQues.length)]
             state.currentWord = randomWord;
             const blankArray = Array(state.currentWord.word.length).fill("_");
             state.blanks = blankArray.join(" ");
-            if(state.lives===0){
+            if (state.lives === 0) {
                 state.score = 0;
                 state.lives = 4;
-            } else if(state.score>0) {
+            } else if (state.score > 0) {
                 state.lives += 1;
             }
             state.allGuesses = [];
         },
-        check_word_guess: (state,{payload}) =>{
-            if(!state.allGuesses.includes(payload.guessedLetter))
+        check_word_guess: (state, { payload }) => {
+            if (!state.allGuesses.includes(payload.guessedLetter))
                 state.allGuesses.push(payload.guessedLetter);
-            
-            if(state.currentWord.word.includes(payload.guessedLetter)){
+
+            if (state.currentWord.word.includes(payload.guessedLetter)) {
                 const blankArray = state.blanks.split(" ");
                 const activeWord = state.currentWord.word.split("");
 
                 activeWord.forEach((char, index) => {
-                    if(char === payload.guessedLetter)
+                    if (char === payload.guessedLetter)
                         blankArray[index] = payload.guessedLetter;
                 });
 
                 state.blanks = blankArray.join(' ');
             } else {
-                if(!state.allGuesses.includes(payload.guessedLetter)){
-                    state.lives -= 1;
-                }
+                state.lives -= 1;
             }
         },
-        check_game_won: (state) =>{
-            if(!state.blanks.includes("_")){
+        check_game_won: (state) => {
+            if (!state.blanks.includes("_")) {
                 state.score += 1;
+                state.allGuesses = Array.from("qwertyuiopasdfghjklzxcvbnm");
+            } else if(state.lives===0){
+                state.allGuesses = Array.from("qwertyuiopasdfghjklzxcvbnm");
             }
+        },
+        diabled_letters: (state) => {
+
         }
     }
 })
 
-export const { initialize_game,check_word_guess,check_game_won } = gameSlice.actions;
+export const { initialize_game, check_word_guess, check_game_won } = gameSlice.actions;
 export default gameSlice.reducer
